20b90a132e3ba61084599f99670a1e793e82b2b8
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2018-06-20 13:37:47 +0200

    Made messier example
    I made the example even messier to make the point clearer.

--- example/example.py
+++ example/example.py
@@ -1,52 +1,51 @@
 import csv
 import os
-
-# Define data directory
-data_dir = "../data"
-
-# Define data file
-fname = "blood_thirsty_janitors.csv"
-
-# Read data
-with open(os.path.join(data_dir, fname)) as csvfile:
-    reader = csv.DictReader(csvfile)
-    total_beard_length = 0
-    total_churches_burned = 0
-    cummulative_age = 0
-    band_members = 0
-    for row in reader:
-        total_beard_length += int(row["beard length"])
-        total_churches_burned += int(row["churches burned"])
-        cummulative_age += int(row["age"])
-        band_members += 1
-
-# Do calculations
-average_beard_length = total_beard_length/band_members
-churches_per_beard_length = total_churches_burned/total_beard_length
-average_age = cummulative_age/band_members
-
+a = "../data"
+b = "blood_thirsty_janitors.csv"
+with open(os.path.join(a, b)) as cf:
+    r = csv.DictReader(cf)
+    temp_1 = 0
+    temp_2 = 0
+    temp_3 = 0
+    temp_4 = 0
+    for rr in r:
+        temp_1 += int(rr["beard length"])
+        temp_2 += int(rr["churches burned"])
+        temp_3 += int(rr["age"])
+        temp_4 += 1
+a_b_l = temp_1/temp_4
+cbl = temp_2/temp_1
+aa = temp_3/temp_4
 print("Calculations done")
-
-
-# Define data file
-fname = "evil_vikings.csv"
-
-# Read data
-with open(os.path.join(data_dir, fname)) as csvfile:
-    reader = csv.DictReader(csvfile)
-    total_beard_length = 0
-    total_churches_burned = 0
-    cummulative_age = 0
-    band_members = 0
-    for row in reader:
-        total_beard_length += int(row["beard length"])
-        total_churches_burned += int(row["churches burned"])
-        cummulative_age += int(row["age"])
-        band_members += 1
-
-# Do calculations
-average_beard_length = total_beard_length/band_members
-churches_per_beard_length = total_churches_burned/total_beard_length
-average_age = cummulative_age/band_members
-
+b = "evil_vikings.csv"
+with open(os.path.join(a, b)) as cf:
+    r = csv.DictReader(cf)
+    temp_1 = 0
+    temp_2 = 0
+    temp_3 = 0
+    temp_4 = 0
+    for rr in r:
+        temp_1 += int(rr["beard length"])
+        temp_2 += int(rr["churches burned"])
+        temp_3 += int(rr["age"])
+        temp_4 += 1
+a_b_l = temp_1/temp_4
+cbl = temp_2/temp_1
+aa = temp_3/temp_4
+print("Calculations done")
+b = "hungry_crusaders.csv"
+with open(os.path.join(a, b)) as cf:
+    r = csv.DictReader(cf)
+    temp_1 = 0
+    temp_2 = 0
+    temp_3 = 0
+    temp_4 = 0
+    for rr in r:
+        temp_1 += int(rr["beard length"])
+        temp_2 += int(rr["churches burned"])
+        temp_3 += int(rr["age"])
+        temp_4 += 1
+a_b_l = temp_1/temp_4
+cbl = temp_2/temp_1
+aa = temp_3/temp_4
 print("Calculations done")
